8a0f9b17311342a51de1b523418f70327ba04bc3
millennial <ereskarasal94@gmail.com>
2024-07-09 19:43:11 +0500

    added alembic for sqlite

---
+++ src/auth/models.py
@@ -0,0 +1,37 @@
+import datetime
+
+from sqlalchemy import (
+    Column,   
+    ForeignKey, 
+    Integer,
+    JSON,
+    MetaData,
+    String,
+    Table,
+    TIMESTAMP
+    )
+
+
+metadata = MetaData()
+
+
+role = Table(
+    "role",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON)
+)
+
+
+user = Table(
+    "user",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False, unique=True),
+    Column("full_name", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.datetime.utcnow, nullable=False),
+    Column("is_active", Integer, nullable=False),
+    Column("role_id", Integer, ForeignKey("role.id"), nullable=False),
+)
