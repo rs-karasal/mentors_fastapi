cc464becfb0cdd196013072823406e52a0a28b8a
millennial <ereskarasal94@gmail.com>
2024-07-13 18:30:08 +0500

    created abstract model for DRY

---
+++ src/abstracts/models.py
@@ -0,0 +1,9 @@
+from datetime import datetime, UTC
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class AbstractModel:
+    created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now(UTC))
+    last_updated: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now(UTC), onupdate=datetime.now(UTC))
+    deleted_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, nullable=True)
