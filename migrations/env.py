c272faa52ef98b12485295e229c9fd31717b06ba
millennial <ereskarasal94@gmail.com>
2024-07-14 01:11:20 +0500

    added models imports

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.auth.models import metadata
+from src.database import Base
+from src.auth.models import *
 
 
 # this is the Alembic Config object, which provides
@@ -21,7 +22,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [
+    Base.metadata
+]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
