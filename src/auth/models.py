41e10a30db5b2b5b83d34a4ae5140d74c617cc3a
millennial <ereskarasal94@gmail.com>
2024-07-15 14:01:21 +0500

    edited permissions nullable=True, also fixed relationships between tables

--- src/auth/models.py
+++ src/auth/models.py
@@ -12,17 +12,17 @@ class AccountType(Base):
     There are different types of accounts, each with different permissions.
     For example: mentee (only learn), mentor (learn and teach), admin (manage mentees and mentors).
     """
-    __tablename__ = "account_type"
+    __tablename__ = "account_types"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(length=100), nullable=False)
-    permissions: Mapped[str] = mapped_column(String(length=100), nullable=False)
+    permissions: Mapped[str] = mapped_column(String(length=100), nullable=True)
 
-    users = relationship("User", back_populates="account_type")
+    user = relationship("User", back_populates="account_types")
 
 
 class User(SQLAlchemyBaseUserTable[int], Base, AbstractModel):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
@@ -30,9 +30,11 @@ class User(SQLAlchemyBaseUserTable[int], Base, AbstractModel):
     last_name: Mapped[str] = mapped_column(String(length=100), nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
     registered_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, nullable=False, default=datetime.now(UTC))
-    account_type_id: Mapped[int] = mapped_column(Integer, ForeignKey("account_type.id"), nullable=False, default=1)
+    account_type_id: Mapped[int] = mapped_column(Integer, ForeignKey("account_types.id"), nullable=False, default=1)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
-    account_type = relationship("AccountType", back_populates="users")
+    account_types = relationship("AccountType", back_populates="user")
+    # messages = relationship("Message", back_populates="user")
+    # chat_member = relationship("ChatMember", back_populates="user")
