a42f54a323b96d68277b4814e7447e07c1c3ba6b
millennial <ereskarasal94@gmail.com>
2024-07-16 19:40:34 +0500

    some little code changes from ruff

--- src/abstracts/models.py
+++ src/abstracts/models.py
@@ -5,5 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class AbstractModel:
     created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now(UTC))
-    last_updated: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now(UTC), onupdate=datetime.now(UTC))
+    last_updated: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP, default=datetime.now(UTC), onupdate=datetime.now(UTC)
+    )
     deleted_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, nullable=True)
--- src/auth/models.py
+++ src/auth/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, UTC
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import Integer, String, TIMESTAMP, Boolean, ForeignKey, Column
+from sqlalchemy import Integer, String, TIMESTAMP, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.abstracts.models import AbstractModel
@@ -12,6 +12,7 @@ class AccountType(Base):
     There are different types of accounts, each with different permissions.
     For example: mentee (only learn), mentor (learn and teach), admin (manage mentees and mentors).
     """
+
     __tablename__ = "account_types"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -25,16 +26,22 @@ class User(SQLAlchemyBaseUserTable[int], Base, AbstractModel):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
     first_name: Mapped[str] = mapped_column(String(length=100), nullable=False)
     last_name: Mapped[str] = mapped_column(String(length=100), nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
-    registered_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, nullable=False, default=datetime.now(UTC))
-    account_type_id: Mapped[int] = mapped_column(Integer, ForeignKey("account_types.id"), nullable=False, default=1)
+    registered_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP, nullable=False, default=datetime.now(UTC)
+    )
+    account_type_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("account_types.id"), nullable=False, default=1
+    )
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
     account_types = relationship("AccountType", back_populates="user")
-    # messages = relationship("Message", back_populates="user")
-    # chat_member = relationship("ChatMember", back_populates="user")
+    messages = relationship("Message", back_populates="user")
+    chats = relationship("ChatMember", back_populates="user")
